a957fad74d134edffc566ed59628a67be243d8e7
overman <overman.robert@gmail.com>
2011-05-26 11:36:10 -0400

    a lot

--- lastCrusadeMain.js
+++ lastCrusadeMain.js
@@ -15,21 +15,25 @@ dojo.declare('lastCrusadeMain', null, {
         var def = uow.getAudio({defaultCaching: true});    //get JSonic
         def.then(dojo.hitch(this, function(audio) { 
             this._audio = audio;
+            this._initSounds();
             this._start();
             dojo.connect(dojo.global, 'onkeyup', dojo.hitch(this, '_removeKeyDownFlag'));
             dojo.connect(dojo.global, 'onkeydown', dojo.hitch(this, '_analyzeKey'));     
             this._keyHasGoneUp = true;
-        }));
+        }));        
     },
 
     _start: function(){
         this.mapList = ["graveyard.json", "forest.json", "castle.json"];
-        this._initMap(this.oneOf(this.mapList));
-        
+        //start background and loop indefinitely 
+        this._audio.setProperty({name: 'loop', channel: 'background', value: true});
+        this.playNow(this.theme, 'background');
+        this.playNow(this.menu, 'main');
+                
     },
 
     //Load a map
-    _initMap: function(fileName) {
+    _loadMap: function(fileName) {
         var file = fileName;
         var mapRequest = {
             url : "games/" + file,
@@ -55,25 +59,156 @@ dojo.declare('lastCrusadeMain', null, {
         }
 	},    
 
+    _initSounds: function(){
+        this.theme = "main_theme";			
+	    this.title = "title";				
+	    // Story										
+	    this.story = "introduction";		
+	    this.ending = "end_king";			
+	    // Menu											
+	    this.menu = "menu";					
+	    this.nodata = "no_save_data";		
+	    this.nocust = "no_save_custom";		
+	    // Directions									
+	    this.dirChar = "dir_characters";	
+	    this.dirEnem = "dir_enemy";			
+	    this.dirItem = "dir_items";			
+	    this.dirLoca = "dir_location";		
+	    this.dirSave = "dir_save";			
+	    this.dirSkip = "dir_skip";			
+	    this.dirSpac = "dir_spacebar";		
+	    this.dirQuit = "dir_quit";			
+	    // Movement										
+	    this.entering = "entering";			
+	    this.heading = "g_heading";			
+	    this.dNorth = "g_north";			
+	    this.dSouth = "g_south";			
+	    this.dEast = "g_east";				
+	    this.dWest = "g_west";				
+	    // Node query									
+	    this.nPass = "g_northern";			
+	    this.sPass = "g_southern";			
+	    this.ePass = "g_eastern";			
+	    this.wPass = "g_western";			
+	    this.hasbeen = "g_visited";			
+	    this.isopen = "g_open";				
+	    this.locked = "g_locked";			
+	    this.need = "g_need";				
+	    this.through = "g_through";			
+	    this.leads = "g_leadto";			
+	    this.vendor = "g_vendor";			
+	    // Item search									
+	    this.noitems = "g_noitems";			
+	    this.nomore = "g_nomore";			
+	    this.found = "g_found";				
+	    this.replace = "g_replace";			
+        //had to change to With vs with	    
+        this.With = "g_with";				
+	    this.equipped = "g_equipped";		
+	    // Battle										
+	    this.fightsong = "fight";			
+	    this.encounter = "encountered";		
+	    this.attack = "enemy_a";			
+	    this.defense = "enemy_d";			
+	    this.hehas = "enemy_h";				
+	    this.nowhas = "enemy_nh";			
+	    this.hitpoints = "hp";				
+	    this.hitpoint = "hp_1";				
+	    this.hitpointsr = "hp_remain";		
+	    this.hitpointr = "hp_remain_1";		
+	    this.runquestion = "run_away";		
+	    this.youlost = "player_l";			
+	    this.younow = "player_nh";			
+	    this.youattack = "player_a";		
+	    this.collected = "collected";		
+	    this.victory = "victory";			
+	    this.easy = "easy_q";				
+	    this.fair = "fair_q";				
+	    this.risky = "risky_q";				
+	    this.enemyMiss = "enemy_m";			
+	    this.playerMiss = "player_m";		
+	    this.playerDeath = "player_x";		
+	    // Vendor										
+	    this.welcome = "v_welcome";			
+	    this.today = "v_today";				
+	    this.purchase = "v_purchase";		
+	    this.vfor = "v_for";				
+	    this.vgold = "v_gold";				
+	    this.nowhave = "v_nowhave";			
+	    this.vgoldr = "v_goldremain";		
+	    this.afford = "v_cannot";			
+	    this.goodbye = "v_goodbye";			
+	    // Friend										
+	    this.talkto = "talkto";				
+	    // Leprechaun									
+	    this.leprechaun = "leprechaun";		
+	    this.lepplay = "play_lep";			
+	    this.lepmore = "more_lep";			
+	    this.lepgone = "lepgone";			
+	    this.leprules = "rules_lep";		
+	    this.lep123 = "123_lep";			
+	    this.lepwin = "pwin_lep";			
+	    this.leplose = "plose_lep";			
+	    this.lepagain = "again_lep";		
+	    this.lepbye = "goodbye_lep";		
+	    this.lepgood = "noplaygood_lep";	
+	    this.lepmad = "noplaymad_lep";		
+	    this.leptook = "leptook";			
+	    this.leplive = "survive_lep";		
+	    this.lepdie = "death_lep";			
+	    // Stat											
+	    this.sndStatStr = "player_s";		
+	    this.sndStatDef = "player_d";		
+	    // Misc											
+	    this.level = "level";				
+	    this.coins = "gold";				
+	    this.sndPotion = "player_p_1";		
+	    this.sndPotions = "player_p";		
+	    this.instruct = "g_instruc";		
+	    this.runyes = "run_yes";			
+	    this.runno = "run_no";				
+	    this.notstrong = "g_notstrong";		
+	    // Save & Load game								
+	    this.load = "loading";				
+	    this.save = "saved";     
+    },
+    
     _analyzeKey: function(evt){
         if (this._keyHasGoneUp) {
         this._keyHasGoneUp = false;              
             result = true;
-            if (this._keyIsDownArrow(evt)) {
+            if (evt.keyCode == dojo.keys.DOWN_ARROW) {
                 evt.preventDefault();
                 result = this.map.move(this.map.SOUTH);
             }
-            else if (this._keyIsLeftArrow(evt)){
+            else if (evt.keyCode == dojo.keys.LEFT_ARROW){
                 evt.preventDefault();
                 result = this.map.move(this.map.WEST);
             }
-            else if (this._keyIsRightArrow(evt)) {
+            else if (evt.keyCode == dojo.keys.RIGHT_ARROW) {
                 evt.preventDefault();
                 result = this.map.move(this.map.EAST);
             }
-            else if (this._keyIsUpArrow(evt)) {
+            else if (evt.keyCode == dojo.keys.UP_ARROW) {
                 evt.preventDefault();
                 result = this.map.move(this.map.NORTH);
+            }
+            else if (evt.keyCode == dojo.keys.NUMPAD_3||
+                     evt.keyCode == 51)
+            {
+                this.readMenu();
+            }
+            else if (evt.keyCode == dojo.keys.NUMPAD_1||
+                     evt.keyCode == 49)
+            {
+                this._masterIndex = 0;
+                var def = this.fadeChannel('background')
+                def.then(dojo.hitch(this, function(){
+                    var def2 = this.fadeChannel('main');
+                    return def2;
+                })).then(dojo.hitch(this, function(){
+                    this._loadMap(this.mapList[this._masterIndex]);
+                })).t;
             }
             if(!result){
                 this._audio.stop({channel: "main"});
@@ -81,52 +216,56 @@ dojo.declare('lastCrusadeMain', null, {
             }
         }        
         else {
-            if (this._keyIsDownArrow(evt) || this._keyIsLeftArrow(evt) || this._keyIsRightArrow(evt) || this._keyIsUpArrow(evt)) {
+            if (evt.keyCode == dojo.keys.UP_ARROW || 
+                evt.keyCode == dojo.keys.RIGHT_ARROW || 
+                evt.keyCode == dojo.keys.LEFT_ARROW || 
+                evt.keyCode == dojo.keys.DOWN_ARROW) 
+            {
                 evt.preventDefault();
             }
             //this._audio.stop({channel: "second"});
             //this._audio.play({url: "sounds/TooEarlyClick", channel : "second"});
         }
     },
+    
+    readMenu: function(){
+        var def = this.fadeChannel('background');
+        def.then(dojo.hitch(this, function(){
+            this.playNow(this.dirSkip, 'main');
+            dojo.forEach([this.dirSpac, this.dirChar, this.dirItem, this.dirLoca, this.dirEnem,   this.dirSave, this.dirQuit, this.instruct, this.menu], dojo.hitch(this, function(sound){
+                this._audio.play({url: "sounds/general/" + sound, channel: 'main'}); 
+            }));
+        }));
+    },
 
-    _keyIsLeftArrow: function(keyStroke) {    //boolean is key pressed left arrow
-        if (keyStroke.keyCode == dojo.keys.LEFT_ARROW) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    /*
+        in future attempt to setup any waiting here
+    */
+    playNow: function(sound, chn){
+        this._audio.stop({channel: chn});
+        this._audio.play({url: "sounds/general/" + sound, channel: chn}); 
     },
 
-    _keyIsRightArrow: function(keyStroke) {    //boolean is key pressed right arrow
-        if (keyStroke.keyCode == dojo.keys.RIGHT_ARROW){
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    
-    _keyIsUpArrow: function(keyStroke) {    //boolean is key pressed left arrow
-        if (keyStroke.keyCode == dojo.keys.UP_ARROW) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    
-    _keyIsDownArrow: function(keyStroke) {    //boolean is key pressed left arrow
-        if (keyStroke.keyCode == dojo.keys.DOWN_ARROW) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    
-    oneOf: function(array){
-        return array[Math.floor(Math.random()*array.length)]
+    /*
+        fade out and stop channel
+    */
+    fadeChannel: function(chn){
+        increments=[0.75, 0.5, 0.25, 0.1, 0.05];
+        i = 0;
+        fadeTimer = new dojox.timing.Timer(400);
+        var deferred = new dojo.Deferred();
+        fadeTimer.onTick = dojo.hitch(this, function() {
+            if(i == (increments.length - 1))
+            {
+                fadeTimer.stop();
+                this._audio.stop({channel : chn});
+                deferred.callback();
+            }
+            this._audio.setProperty({name : 'volume', value: increments[i], channel : chn, immediate : true});
+            i++;
+        });
+        fadeTimer.start();
+        return deferred;
     }
 });
 
